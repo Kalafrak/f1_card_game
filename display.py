3ba58bb4f8cae9935c009740c6cc3327e4effb69
Kalafrak <mdelldeadstar@hotmail.co.uk>
2026-08-01 16:01:20 +0100

    Refactored game_loop.py to only contain logic and helper functions. Moved all printing and time functions to display.py

---
+++ display.py
@@ -0,0 +1,56 @@
+import time
+
+def display_intro():
+    print("Welcome to Apex Legacy!")
+    print("Follow the prompts and try to outscore your opponent in each round.")
+    print("Type 'quit' at any time to leave the game. (Real racing drivers don't quit!)")
+    while True:
+        user_ready = input("Are you ready to race!! (y/n)")
+        if user_ready.lower() in ["quit", "exit", "n", "no"]:
+            print("See ya 'round, Rookie!")
+        elif user_ready.lower() in ["y", "yes"]:
+            print("It's cards out and away we go!!")
+            break
+        print("That's not a valid input; are you sure you warmed up your tyres correctly?")
+
+def display_round_number(round_num):
+    print(f"{'*' * 50}\n{'Round ' + str(round_num):^50}\n{'*' * 50}")
+
+def display_player_card(player_card):
+    print("Your card is:")
+    time.sleep(1.5)
+    print(player_card)
+
+def display_round_result(result, cpu_card, player_score, cpu_score):
+    if result == "player":
+        print("You win this round!")
+    elif result == "cpu":
+        print("Unlucky!")
+    else:
+        print("It's a photo finish!")
+    time.sleep(1.5)
+    print("CPU had:")
+    time.sleep(1.5)
+    print(cpu_card)
+    print(f"Player score: {player_score}    CPU score: {cpu_score}")
+    input("Press Enter to continue...")
+
+def display_end_of_game(player_score, cpu_score):
+    if player_score > cpu_score:
+        input("And there's the chequered flag!")
+        input(f"You clinched the championship by {player_score - cpu_score}!")
+        input("And I've got to stop...")
+        input("because I've got a lump in my throat...")
+        input("You'll go down in history as one of the greats!")
+        input("Let's hope next season's development hasn't been hurt by your tremendous championship run!")
+    
+    elif player_score < cpu_score:
+        input("And there's the chequered flag!")
+        input(f"Your rival beat you to the championship by {cpu_score - player_score}!")
+        input("That's another year of hard work ahead for you!")
+        input("In the meantime, let's hope you don't get replaced by the test driver!")
+    
+    else:
+        input("You've tied the championship! Unheard of!")
+        input("There'll be months of bickering with the FIA about this.")
+        input("Better just go on holiday and forget about it for a while.")
--- game_loop.py
+++ game_loop.py
@@ -1,16 +1,8 @@
-import time
+from display import display_intro, display_round_number, display_player_card, display_round_result, display_end_of_game
 
 def play_game(deck):
 #Intro
-    print("Welcome to Apex Legacy!")
-    print("Follow the prompts and try to outscore your opponent in each round.")
-    print("Type 'quit' at any time to leave the game. (Real racing drivers don't quit!)")
-    user_ready = input("Are you ready to race!! (y/n)")
-    if user_ready.lower() in ["y", "yes"]:
-        print("It's cards out and away we go!!")
-    else:
-        print("See ya 'round, Rookie!")
-        return
+    display_intro()
 
 #Main game loop
     player_score = 0
@@ -24,16 +16,16 @@ def play_game(deck):
         "sc": "scrupulousness", "scrupulousness": "scrupulousness",
     }
     for round_num in range(1, 11):                                          #10 round game length
-        print(f"{'*' * 50}\n{'Round ' + str(round_num):^50}\n{'*' * 50}")   #Print round number
+        display_round_number(round_num)                                     #Print round number
         player_card = deck.draw()                                           #Draw player's card
         cpu_card = deck.draw()                                              #Draw cpu's card
-        print("Your card is:")
-        time.sleep(1.5)
-        print(player_card)
+
+        display_player_card(player_card)
+
 #While loop to make sure user inputs a valid stat.
         while True:
             player_chosen_stat = input("Choose a stat to attack with (ta/lo/ag/mo/hu/sc)")
-            if player_chosen_stat in ["quit", "exit"]:
+            if player_chosen_stat.lower() in ["quit", "exit"]:
                 print("See ya 'round, Rookie!")
                 return
             if player_chosen_stat in stat_map:
@@ -43,52 +35,32 @@ def play_game(deck):
         stat_name = stat_map[player_chosen_stat]                            #Matches user input to the stat_map to feed into getattr()
         player_value = getattr(player_card, stat_name)                      #Gets value from the loaded .json list of cards using key from stat_map.
         cpu_value = getattr(cpu_card, stat_name)
+        result = compare_stat(player_value, cpu_value)
+        change = calculate_score_change(result, player_value, cpu_value)
+        if result == "player":
+            player_score += change
+        elif result == "cpu":
+            cpu_score += change
 
-        if player_value > cpu_value:
-            print("You win this round!")
-            player_score += player_value - cpu_value
-            time.sleep(1.5)
-            print("CPU had:")
-            time.sleep(1.5)
-            print(cpu_card)
-            print(f"Player score: {player_score}    CPU score: {cpu_score}")
-            input("Press Enter to continue...")
-        elif player_value < cpu_value:
-            print("Unlucky!")
-            cpu_score += cpu_value - player_value
-            time.sleep(1.5)
-            print("CPU had:")
-            time.sleep(1.5)
-            print(cpu_card)
-            print(f"Player score: {player_score}    CPU score: {cpu_score}")
-            input("Press Enter to continue...")
-        else:
-            print("It's a photo finish!")
-            time.sleep(1.5)
-            print("CPU had:")
-            time.sleep(1.5)
-            print(cpu_card)
-            print(f"Player score: {player_score}    CPU score: {cpu_score}")
-            input("Press Enter to continue...")
+        display_round_result(result, cpu_card, player_score, cpu_score)
 
-    if player_score > cpu_score:
-        input("And there's the chequered flag!")
-        input(f"You clinched the championship by {player_score - cpu_score}!")
-        input("And I've got to stop...")
-        input("because I've got a lump in my throat...")
-        input("You'll go down in history as one of the greats!")
-        input("Let's hope next season's development hasn't been hurt by your tremendous championship run!")
-        return
+    display_end_of_game(player_score, cpu_score)
 
-    elif player_score < cpu_score:
-        input("And there's the chequered flag!")
-        input(f"Your rival beat you to the championship by {cpu_score - player_score}!")
-        input("That's another year of hard work ahead for you!")
-        input("In the meantime, let's hope you don't get replaced by the test driver!")
-        return
+    return
 
+#Helper functions for logic only
+def compare_stat(player_value, cpu_value):
+    if player_value > cpu_value:
+        return "player"
+    elif player_value < cpu_value:
+        return "cpu"
     else:
-        input("You've tied the championship! Unheard of!")
-        input("There'll be months of bickering with the FIA about this")
-        input("Better just go on holiday and forget about it for a while")
-        return
+        return "tie"
+
+def calculate_score_change(result, player_value, cpu_value):
+    if result == "player":
+        return player_value - cpu_value
+    elif result == "cpu":
+        return cpu_value - player_value
+    else:
+        return 0
